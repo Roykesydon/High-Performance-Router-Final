a0332f644c5552a4eaa39c7e1b2ebec11e810a38
Roykesydon <roy23381547@gmail.com>
2024-06-08 00:44:54 +0800

    refactor: move hyperparameters from main.cpp to this file.

--- constants.h
+++ constants.h
@@ -1,6 +1,15 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
+#define ll long long
 
 const unsigned int RULE_COUNT = 953;
+
+// constraints
+const ll INITIAL_RANGE = 20;
+const ll DELTA = 3;
+const ll MAX_WAITING_PACKETS = 1000;
+const ll MAX_WAITING_FLOWS = 60;
+
+const ll TABLE_ONE_THRESHOLD = 500;
 
 #endif  // CONSTANTS_H
